000dad5ca076f190e9f6be0e044f115fa8f5d920
Ananda <anandagopaldutta2001@gmail.com>
2021-08-15 21:04:50 +0530

    second commit

---
+++ FurnitureStore/project_src/server/config.js
@@ -0,0 +1,8 @@
+//All the configurations required like portNumber, Database Connection URl etc are stored to provide a low level encapsulation
+
+module.exports = {
+  database:
+    "mongodb+srv://FurnitureStoreWebsite:FurnitureStoreWebsitePassword@cluster0.26nuz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  port: 3030,
+  secret: "WPL_FINAL_PROJECT",
+};
